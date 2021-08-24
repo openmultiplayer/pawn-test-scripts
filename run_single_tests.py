ff08bd1882a6ed063f620c619223efa16650953c
The Cheaterman <the.cheaterman@gmail.com>
2021-08-24 19:43:13 +0200

    Use pawncc directly to compile single tests, check all outputs

--- run_single_tests.py
+++ run_single_tests.py
@@ -1,10 +1,11 @@
-import io
 import json
 import pathlib
+import queue
 import re
-import shlex
 import subprocess
 import sys
+import threading
+import time
 
 
 OMP_COMMAND = (
@@ -13,6 +14,103 @@ OMP_COMMAND = (
     else 'omp-server.exe'
     # OSX can fuck off, naturally
 )
+PAWNCC_COMMAND = (
+    './pawncc'
+    if sys.platform == 'linux'
+    else 'pawncc.exe'
+    # Ditto
+)
+DEPENDENCIES = [
+    path
+    for path in pathlib.Path('dependencies').glob('*')
+    if path.is_dir()
+]
+
+
+class ProgramOutput:
+    def __init__(self):
+        self.stdout = queue.Queue()
+        self.stderr = queue.Queue()
+
+    def get_stream_as_string(self, stream_name):
+        stream = getattr(self, stream_name)
+        output = ''
+
+        while True:
+            try:
+                line = stream.get_nowait()
+            except queue.Empty:
+                break
+
+            output += line
+
+        return output
+
+
+class Program:
+    def __init__(self, command, **popen_args):
+        self._process = subprocess.Popen(
+            command,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True,
+            **popen_args
+        )
+
+    def run(self, timeout=None):
+        start_time = time.time()
+        output = ProgramOutput()
+
+        with self._process as process:
+            watch_threads = []
+
+            for stream, stream_queue in (
+                (process.stdout, output.stdout),
+                (process.stderr, output.stderr),
+            ):
+                thread = threading.Thread(
+                    target=self._watch,
+                    args=(stream, stream_queue),
+                )
+                watch_threads.append(thread)
+                thread.start()
+
+            while(
+                timeout is None
+                or time.time() - start_time < timeout
+            ):
+                if process.poll() is not None:
+                    break
+
+                time.sleep(.01)
+
+            else:
+                process.terminate()
+                process.communicate()
+                was_terminated = True
+
+            if(
+                process.returncode != 0
+                and not was_terminated
+            ):
+                process_name = ' '.join(process.args)
+                raise RuntimeError(
+                    f'Process "{process_name}" failed '
+                    f'with return code {process.returncode}'
+                )
+
+        for thread in watch_threads:
+            thread.join()
+
+        return output
+
+    def _watch(self, stream, stream_queue):
+        try:
+            for line in iter(stream.readline, ''):
+                stream_queue.put(line)
+        except ValueError:  # I/O operation on closed file
+            pass
 
 
 def main():
@@ -31,49 +129,58 @@ def main():
         json_data_path = folder / f'{module}.json'
 
         if not json_data_path.exists():
-            print(f'No JSON test data found for {source_file}, ignoring...')
+            print(f'No JSON test data found for {source_file}, skipping...')
             continue
 
-        subprocess.run(
-            shlex.split(f'sampctl package build {module}'),
-            capture_output=True,
-            check=True,
-        )
-
         amx_path = folder / f'{module}.amx'
+        amx_path.unlink(missing_ok=True)
+
+        pawncc = Program([
+            PAWNCC_COMMAND,
+            source_file.resolve(),
+            f'-o{amx_path.resolve()}',
+            '-Z+',
+            *[
+                f'-i{dependency.resolve()}'
+                for dependency in DEPENDENCIES
+            ]
+        ]).run()
 
         if not amx_path.exists():
             print(f'File {source_file} failed to build, aborting.')
             return 1
 
         with json_data_path.open() as json_data_file:
-            json_data = json.load(json_data_file)
-
-        expected_output_str = json_data['output']
-        expected_output_re = re.compile(expected_output_str)
-        expected_output_lines = expected_output_str.count('\n') + 1
+            expected_data = json.load(json_data_file)
 
         print(f'Running tests for {module}...')
 
-        try:
-            subprocess.run(
-                [OMP_COMMAND, str(amx_path)],
-                capture_output=True,
-                check=True,
-                timeout=.5,
-            )
-        except subprocess.TimeoutExpired as exception:
-            output = exception.stderr.decode().strip()
+        omp = Program([
+            OMP_COMMAND,
+            str(amx_path)
+        ]).run(timeout=.5)
 
-        useful_lines = output.split('\n')[-expected_output_lines:]
-        useful_output = '\n'.join(useful_lines)
+        for program_name, program in {
+            'omp': omp,
+            'pawncc': pawncc,
+        }.items():
+            for stream_name in (
+                'stdout',
+                'stderr',
+            ):
+                output = program.get_stream_as_string(stream_name).rstrip('\n')
+                expected_pattern = expected_data[program_name][stream_name]
+                expected_output_lines = expected_pattern.count('\n') + 1
+                useful_lines = output.split('\n')[-expected_output_lines:]
+                useful_output = '\n'.join(useful_lines)
 
-        if not expected_output_re.fullmatch(useful_output):
-            print(
-                f'Tests for {module} failed: '
-                f'Mismatching input {useful_output!r}'
-            )
-            return 1
+                if not re.fullmatch(expected_pattern, useful_output):
+                    print(
+                        f'Tests for {module} failed '
+                        f'(mismatch on {program_name} {stream_name}): '
+                        + repr(useful_output)
+                    )
+                    return 1
 
         print(f'Tests for {module} succeeded.')
 
